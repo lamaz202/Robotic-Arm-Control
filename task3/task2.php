71b8429e84cb8c0dd01885c2053d40357ff350d4
lamaz202 <122086775+lamaz202@users.noreply.github.com>
2023-08-08 15:36:43 +0300

    Add files via upload

---
+++ task3/task2.php
@@ -0,0 +1,25 @@
+<?php
+$button=$_POST["s"];
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "text";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO n (id , text)
+VALUES ('', '$button')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
