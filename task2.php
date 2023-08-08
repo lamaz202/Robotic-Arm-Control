87adabda8625c58baac2bb0a5ea4a6cfb87b8bd0
lamaz202 <122086775+lamaz202@users.noreply.github.com>
2023-08-08 10:50:59 +0300

    Update task2.php

--- task2.php
+++ task2.php
@@ -1,25 +1,25 @@
 <?php
+$button=$_POST["s"];
 $servername = "localhost";
-$username = "your_username";
-$password = "your_password";
+$username = "root";
+$password = "";
 $dbname = "text";
 
-// Create a new database connection
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
-// Get the text from the POST request
-$text = $_POST['text'];
+$sql = "INSERT INTO n (id , text)
+VALUES ('', '$button')";
 
-// Prepare and execute the SQL statement
-$stmt = $conn->prepare("INSERT INTO texts (text) VALUES (?)");
-$stmt->bind_param("s", $text);
-$stmt->execute();
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
-$stmt->close();
 $conn->close();
 ?>
